0b66f8e6ecef8cc2f13ffd4d3f51db993be60ccd
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-06-01 15:30:09 +0900

    address rivised3

--- frontend/src/components/TodayMenu.js
+++ frontend/src/components/TodayMenu.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Navigation from "./Navigation";
+import { useState,useEffect } from 'react'
 
 
 
 const TodayMenu = () => {
+
+
+  const [message, setMessage] = useState('');
+  useEffect(() =>{
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  },[])
+
 
   const handleClick = async () => {
     await fetch("/img", {
@@ -21,7 +31,7 @@ const TodayMenu = () => {
   return (
     <div>
     <Navigation />
-    
+    <p>{message}</p>
     <button onClick={handleClick}>post</button>
       本日のメニューを動的に追加及び削除できるシステムで管理者のみ変更できるようにしたい
     </div>
