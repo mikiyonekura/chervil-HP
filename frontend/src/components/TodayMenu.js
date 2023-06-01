5d96a06c0c1edbb354d8f89f52accca79f3efdf7
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-06-01 15:20:25 +0900

    address rivised2

--- frontend/src/components/TodayMenu.js
+++ frontend/src/components/TodayMenu.js
@@ -6,7 +6,7 @@ import Navigation from "./Navigation";
 const TodayMenu = () => {
 
   const handleClick = async () => {
-    await fetch("../img", {
+    await fetch("/img", {
       method: "POST",
       mode: "cors",
       headers: {
@@ -21,6 +21,7 @@ const TodayMenu = () => {
   return (
     <div>
     <Navigation />
+    
     <button onClick={handleClick}>post</button>
       本日のメニューを動的に追加及び削除できるシステムで管理者のみ変更できるようにしたい
     </div>
