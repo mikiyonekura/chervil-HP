530bb20e9c524053c19b0c053f8c731d0ea0fc4e
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-06-04 16:19:15 +0900

    added input function and delete function2

--- backend/index.js
+++ backend/index.js
@@ -19,38 +19,42 @@ app.use((req, res, next) => {
     next();
   });
 
+  database= [
+    {
+      id:1,
+      name:"りんご",
+      price:200,
+      image:"https://source.unsplash.com/gDPaDDy6_WE",
+    },
+    {
+      id:2,
+      name:"バナナ",
+      price:300,
+      image:"https://source.unsplash.com/zrF6ACPLhPM",
+    },
+    {
+      id:3,
+      name:"みかん",
+      price:"150",
+      image:"https://source.unsplash.com/bogrLtEaJ2Q",
+    },
+    {
+      id:4,
+      name:"メロン",
+      price:"2000",
+      image:"https://source.unsplash.com/8keUtGmy0xo",
+    },
+  ];
+
+
 app.get("/fruits", (req , res) => {
-    res.json([
-      {
-        id:1,
-        name:"りんご",
-        price:200,
-        image:"https://source.unsplash.com/gDPaDDy6_WE",
-      },
-      {
-        id:2,
-        name:"バナナ",
-        price:300,
-        image:"https://source.unsplash.com/zrF6ACPLhPM",
-      },
-      {
-        id:3,
-        name:"みかん",
-        price:"150",
-        image:"https://source.unsplash.com/bogrLtEaJ2Q",
-      },
-      {
-        id:4,
-        name:"メロン",
-        price:"2000",
-        image:"https://source.unsplash.com/8keUtGmy0xo",
-      },
-    ]);
+    res.json(database[0]);
 });
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+
 
 // postの処理
 app.post("/img", function (req, res) {
@@ -58,6 +62,7 @@ app.post("/img", function (req, res) {
       console.log("通信きたよー");
       console.log(req.body)
       res.json(req.body.nameOfSweets + "の画像を返したいよ")
+      //res.json(database[0])
       
       /*res.json(req.body); */ // jsonで返却
   
